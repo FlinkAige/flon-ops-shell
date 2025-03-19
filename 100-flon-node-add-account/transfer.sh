46506f3a0e42d30d4a0996017caf0536deb4861a
Joss <281900360@qq.com>
2025-03-19 21:21:56 +0800

    update

--- 100-flon-node-add-account/transfer.sh
+++ 100-flon-node-add-account/transfer.sh
@@ -1,8 +1,11 @@
+shopt -s expand_aliases
+source ~/.bashrc
 focli wallet unlock -n test --password $1
+
 acc=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 12 | head -n 1)
 tcli system newaccount flon $acc FO8ixPk3x4wZQu1bwBtw67JznFr5LVcA9bfDpkS7grnms3JNm7Qq  --transfer-quant "10.000000 FLON" -p flon
 sleep 3
 random_string=$(cat /dev/urandom | tr -dc '1-9' | fold -w 3 | head -n 1)
 result=$(echo "scale=2; $random_string / 100" | bc)
 
-focli transfer flon $acc "${result} FLON"
+tcli transfer flon $acc "${result} FLON"
