d82d0900d7d847eabb66f9424068a4d309dfc427
FlinkStone <161567007+FlinkAige@users.noreply.github.com>
2025-03-24 15:53:33 +0800

    Update transfer.sh

--- 100-flon-node-add-account/transfer.sh
+++ 100-flon-node-add-account/transfer.sh
@@ -3,7 +3,7 @@ source ~/.bashrc
 unlockt
 
 acc=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 12 | head -n 1)
-tcli system newaccount flon $acc FU8ixPk3x4wZQu1bwBtw67JznFr5LVcA9bfDpkS7grnms3JNm7Qq  --transfer-quant "10.00000000 FLON" -p flon
+tcli system newaccount flon $acc FU8ixPk3x4wZQu1bwBtw67JznFr5LVcA9bfDpkS7grnms3JNm7Qq  --fund-account "10.00000000 FLON" -p flon
 sleep 3
 random_string=$(cat /dev/urandom | tr -dc '1-9' | fold -w 3 | head -n 1)
 result=$(echo "scale=2; $random_string / 100" | bc)
