1c87372a18207ff56d3f67f0d486c94027650ba9
Joss <281900360@qq.com>
2025-03-30 10:20:32 +0800

    update

--- 200-reset-node/reset_wal.sh
+++ 200-reset-node/reset_wal.sh
@@ -1,6 +1,6 @@
-CONTAINER_NAME=flon_wal
+CONTAINER_NAME=fuwal
 
-DATA_DIR="~/$CONTAINER_NAME"
+DATA_DIR="~/"
 
 # Stop and remove the container if it exists
 if docker ps -a | grep -q "$CONTAINER_NAME"; then
