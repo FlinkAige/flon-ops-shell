55dbbcfa5b51854957a5ccd96f7162f2bc07a10a
Joss <281900360@qq.com>
2025-04-02 10:07:59 +0800

    update code style

--- 200-reset-node/2_build_contract.sh
+++ 200-reset-node/2_build_contract.sh
@@ -1,9 +1,14 @@
 
-CONTRACT_PATH=$1
+CONTRACT_PATH=$1/contracts
+CONTRACT_PATH=/opt/data/flon_devnet_02/contracts
+
 # 安装依赖
+apt update
 apt install -y libssl-dev libboost-all-dev libgmp3-dev libbz2-dev libreadline-dev libncurses5-dev libusb-1.0-0-dev libudev-dev libusb-dev libusb-1.0-0
 apt install libcurl4-gnutls-dev cmake -y
 
+cd $CONTRACT_PATH/
+apt-get install -y ./flon.cdt_0.3.0-alpha_amd64.deb
 
 cd $CONTRACT_PATH/flon.contracts
 ./build.sh -y
